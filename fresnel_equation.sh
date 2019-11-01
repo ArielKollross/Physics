c6a3ecbb2f929cbf15500ccf5d68eb22d71e4f32
Ariel Kollross <arielkollross@gmail.com>
2019-11-01 11:18:29 -0300

    Add files via upload

--- fresnel_equation.sh
+++ fresnel_equation.sh
@@ -1,28 +1,33 @@
 #! /usr/bin/gnuplot -p
 
- #Gnuplot script Fresnel equation
- set term postscript eps enhanced color lw 2 "Arial" 20
- set title "Fresnel do ar para vidro"
+#Gnuplot script Fresnel equation
+set term postscript eps enhanced color lw 2 "Arial" 20
+set title "Fresnel do ar para vidro"
 #set name for file
- set output "fresnel.eps"
- #Define refractive index middle 1
- ni = 
- #Define refractive index middle 2
- nt =   
- #Define alpha a = u1*v1/u2*v2, if u1=u2, then a = nt/ni
- a = nt/ni
- #Define b = cos(t)/cos(i)
- b = cos(t) /cos(i)
+set output "fresnel.eps"
+#Define refractive index middle 1
+ni = 1
+#Define refractive index middle 2
+nt =  3/2
+#Define constatn
+k = ni/nt
+#Define conversion
+TRAD = pi/180.0
+#define cos, sin conversion
+sind(x) = sin(x*TRAD)
+cosd(x) = cos(x*TRAD)
+#Define sqrt
+sq(x) = sqrt( 1 - k**2 * sind(x)*sind(x) )
+sqd(x) = sq(x*TRAD)
 #R perpendicular
-ri = ( (1-ab)/(1 + ab) ) **2
+ri(x) = (ni*cosd(x) - nt*sqd(x)) / (ni*cosd(x) + nt * sqd(x))
 #R paralelo
-rii = ((b-a)/(a+b))**2
+rii(x) = - (nt*cosd(x) - ni*sqd(x)) / (ni*sqd(x) + nt*cosd(x))
 #Function
-P(i) = (ri - rii) / (ri + rii)
+p(x) = (ri(x) - rii(x)) / (ri(x) + rii(x))
 
- set ylabel ""
-
- set xlabel "Angulo de incidência"
- #ploting graph
- plot P(i)
-  
+set ylabel "ress"
+set xlabel "Angulo de incidencia"
+set yrange [-1:1]
+#ploting graph
+plot p(x) with line
