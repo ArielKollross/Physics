1be163990a3c0ee8a5dc16a844b0cb56fa1f9150
Ariel Kollross <arielkollross@gmail.com>
2019-11-03 15:17:58 -0300

    Add files via upload

--- graphics/fresnel_equation.sh
+++ graphics/fresnel_equation.sh
@@ -1,34 +1,48 @@
 #! /usr/bin/gnuplot -p
 
 #Gnuplot script Fresnel equation
+set encoding utf8
 set term postscript eps enhanced color lw 2 "Arial" 20
-set title "Fresnel do ar para vidro"
+set title "Ar para Vidro"
+#set title "Vidro para Ar"
 #set name for file
 set output "fresnel.eps"
 #Define refractive index middle 1
-ni = 1
-#Define refractive index middle 2
-nt =  3/2
-#Define constatn
-k = ni/nt
-#Define conversion
-TRAD = pi/180.0
-#define cos, sin conversion
-sind(x) = sin(x*TRAD)
-cosd(x) = cos(x*TRAD)
-#Define sqrt
-sq(x) = sqrt( 1 - k**2 * sind(x)*sind(x) )
-sqd(x) = sq(x*TRAD)
-#R perpendicular
-ri(x) = (ni*cosd(x) - nt*sqd(x)) / (ni*cosd(x) + nt * sqd(x))
-#R paralelo
-rii(x) = - (nt*cosd(x) - ni*sqd(x)) / (ni*sqd(x) + nt*cosd(x))
-#Function
-p(x) = (ri(x) - rii(x)) / (ri(x) + rii(x))
+ni = 1.0 # ar-vidro
+#ni = 3.0/2.0 #vidro-ar
 
-set ylabel "ress"
-set xlabel "Angulo de incidencia"
-set yrange [-1:2.5]
-set autoscale x
+#Define refractive index middle 2
+nt = 3.0/2.0 # ar-vidro
+#nt =  1.0 # vidro-ar
+
+#Define constant
+k = ni/nt
+#Define sqrt
+sq(x) = sqrt( 1 - k**2 * sin(x)*sin(x) )
+#R perpendicular
+ri(x) = (ni*cos(x) - nt*sq(x)) / (ni*cos(x) + nt * sq(x))
+#R paralelo
+rii(x) = - (nt*cos(x) - ni*sq(x)) / (ni*sq(x) + nt*cos(x))
+#t perpendicular
+ti(x) = ( 2*ni* cos(x) ) / ( ni*cos(x) + nt*sq(x) )
+#t paralelo
+tii(x) = (2*ni* cos(x) ) / ( ni*sq(x) + nt*cos(x) )
+
+#Function
+p(x) = ( ri(x)*ri(x) - rii(x)*rii(x) ) / ( ri(x)*ri(x) + rii(x)*rii(x))
+
+set xlabel "Angulo de incidência"
+set yrange [-1.1:2.5]
+set xrange [0:1]
+
+#For condition ni = 1
+set arrow from 1.571, graph 0 to 1.571, graph 1 nohead
+#For condition ni = 1.5
+#set arrow from 0.729, graph 0 to 0.729, graph 1 nohead 
+
 #ploting graph
-plot p(x), ri(x) with line, rii(x) w line
+plot ri(x) with line title 'r {/=10{/Symbol \136}}', \
+rii(x) w line title 'r {/=10{/Symbol \174\174}}', \
+ti(x) w line title 't {/=10{/Symbol \136}}', \
+tii(x) w line title 't {/=10{/Symbol \174\174}}'
+
