2b18eb8fec39eee5dc2cca024e52e0a9dd5a316b
Ariel Kollross <arielkollross@gmail.com>
2019-12-13 21:50:19 -0300

    Add files via upload

---
+++ graphics/p3_exe2.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/gnuplot -p
+set encoding utf8
+set term postscript eps enhanced color lw 2 "Arial" 20
+set output "p3exe2.eps"
+
+
+#Define constants
+c = 3e8
+u = (4e-7 )*pi
+j = 1e-2/2.0
+
+A = (c*u*(j)**2 )/ 23*pi**2
+#function
+f(x) = A *( (cos(pi*cos(x))+1 ) / sin(x) )**2
+g(x) = (A/1.09 ) *( (cos(pi*cos(x))+1 ) / sin(x) )**2
+
+set grid 
+set xlabel "Potência"
+set xlabel " {/Symbol q} [rad]"
+#set yrange [0:1]
+set xrange [0:3]
+
+plot f(x) with line lt rgb "#000000" title 'Solução Exata', \
+g(x) with line lt rgb "red" title 'Solução Aproximada'
